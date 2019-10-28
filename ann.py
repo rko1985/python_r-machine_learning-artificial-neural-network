98578389afcc675b08a020788609442505105d97
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-10-28 10:26:36 -1000

    R template finished

--- ann.py
+++ ann.py
@@ -1,21 +1,12 @@
 # Artificial Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
+# Data preprocessing
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#PART 1 DATA PREPROCESSING
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
@@ -29,11 +20,11 @@ labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
+X = X[:, 1:] #getting rid of dummy variable
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -41,32 +32,25 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
-
-# Importing the Keras libraries and packages
+# PART 2 MAKING THE ANN
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Initialising the ANN
+# Initializing the ANN
 classifier = Sequential()
-
 # Adding the input layer and the first hidden layer
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
-
-# Adding the second hidden layer
+# Adding second hidden layer
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
-
 # Adding the output layer
 classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 
 # Compiling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Fitting the ANN to the Training set
+# Fitting the ANN  to the training set 
 classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
-
-# Part 3 - Making the predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
